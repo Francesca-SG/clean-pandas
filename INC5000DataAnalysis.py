7916fb7c78dc6ac452280e9eb132a1791291cd69
Francesca-SG <133805548+Francesca-SG@users.noreply.github.com>
2024-06-02 12:13:55 +0100

    Replaced values and converted data types
    Replaced values and converted data types for two columns. Used an if statement to determine what the value was before removing any unnecessary data, before converting and doing any appropriate calculations.
    
    Cleaned up the code, currently converting the different processes into functions for readability and scalability.

--- INC5000DataAnalysis.py
+++ INC5000DataAnalysis.py
@@ -7,16 +7,15 @@ import numpy as np
 import data_checks
 
 
-# Gets the directory of the current script
-script_dir = os.path.dirname(os.path.realpath(__file__))
+# Function gets directory of current script, builds the path to the file, and read files
+def read_data(file_name):
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(script_dir, file_name)
+    # Data Frame (df) is a 2D data structure, like an array or table
+    df = pd.read_csv(file_path)
+    return df
 
-# Builds the path to the file
-file_path = os.path.join(script_dir, 'INC5000_Companies_2019.csv')
-
-# Reads the CSV file
-# Data Frame (df) is a 2 dimensional data structure, like an array or table, with columns and rows
-df = pd.read_csv(file_path)
-
+df = read_data('INC5000_Companies_2019.csv')
 
 # Renames columns in table and assigns it back to the dataframe, saving the changes
 df = df.rename(columns={'workers':'current_worker_count', 'previous_workers':'previous_worker_count', 'name':'company_name'})
@@ -25,6 +24,8 @@ df = df.rename(columns={'workers':'current_worker_count', 'previous_workers':'pr
 df['current_worker_count']= df['current_worker_count'].fillna(df['current_worker_count'].mean())
 df['metro'] = df['metro'].fillna('Not Applicable') 
 
+# Converts column data type to integers
+df['current_worker_count']= df['current_worker_count'].astype(int)
 
 # Returns boolean value for each row
 # Print(df.duplicated())
@@ -43,16 +44,31 @@ data_checks.empty_dataframe(df)
 # Check if the 'current_worker_count' column exists and contains numerical data
 data_checks.is_empty_column(df)
 
-def convert_revenue(value):
-    if 'Million' in value.lower():
-        # Removes 'Million', converts to float and mulitiplies by 1e6
-        value = value.replace('Million','').strip()
-        return float(value) * 1e6
 
-# Apply the function to the 'revenue' column   
+# This codw would have been fine if it were only used to replace one value
+#def convert_revenue(value):
+#df['revenue'] = df['revenue'].astype(str).replace(['Million', 'Billion'], '', regex=True)
+#df['revenue'] = df['revenue'].astype(str).str.replace('Billion', '')
+#df['revenue'] = df['revenue'].astype(float)
+#df['revenue'] = df['revenue'] * 1e6 
+
+# Replaces the values and converts to float, multiplies for correct value
+def convert_revenue(value):
+    value = value.replace(',', '')
+    if 'Billion' in value:
+        value = value.replace('Billion', '').strip()
+        return float(value) * 1e9
+    elif 'Million' in value:
+        value = value.replace('Million', '').strip()
+        return float(value) * 1e6
+    else:
+        return float(value)
+
 df['revenue'] = df['revenue'].apply(convert_revenue)
 
-##Bug here, revenue column returning none I think I know how to fix.
+#dt = df.dtypes
+#print(dt)
+
 
 #z_scores = np.abs(stats.zscore(df['current_worker_count']))
 # This will show the outliers
